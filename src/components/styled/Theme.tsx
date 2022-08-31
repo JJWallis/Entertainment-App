13ad9921936ede5eb6a30832f95d4c79b77114fb
JJWallis <josh.jamesonwallis@gmail.com>
2022-08-31 18:14:21 +0100

    font sizes added to theme

--- src/components/styled/Theme.tsx
+++ src/components/styled/Theme.tsx
@@ -1,6 +1,20 @@
 declare module 'styled-components' {
    export interface DefaultTheme {
       breakPoints: { mobile: string; tablet: string; desktop: string }
+      colors: {
+         brightRed: string
+         deepBlue: string
+         darkBlue: string
+         lightBlue: string
+         white: string
+      }
+      fontSizes: {
+         bodySmall: string
+         bodyLarge: string
+         headingExtraSmall: string
+         headingSmallMedium: string
+         headingLarge: string
+      }
    }
 }
 
@@ -12,5 +26,12 @@ export const Theme = {
       darkBlue: '#161D2F',
       lightBlue: '#5A698F',
       white: '#FFFFFF',
+   },
+   fontSizes: {
+      bodySmall: '13px',
+      bodyLarge: '15px',
+      headingExtraSmall: '18px',
+      headingSmallMedium: '24px',
+      headingLarge: '32px',
    },
 }
