333c603e32d201c2294e285e82d775842d87f1d9
JJWallis <josh.jamesonwallis@gmail.com>
2022-08-31 19:01:56 +0100

    login button styles

---
+++ src/components/styled/Button.styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const LoginButton = styled.button`
+   display: block;
+   border-radius: 5px;
+   padding-block: 10px;
+   text-align: center;
+   background-color: ${({ theme }) => theme.colors.brightRed};
+   color: ${({ theme }) => theme.colors.white};
+   transition: background-color, color 400ms ease-in;
+
+   :hover {
+      background-color: ${({ theme }) => theme.colors.white};
+      color: ${({ theme }) => theme.colors.deepBlue};
+   }
+`
