a3bbe03217d20d748c5b942d31063ff56d67561d
JJWallis <josh.jamesonwallis@gmail.com>
2022-08-31 18:09:11 +0100

    colors added to theme

--- src/components/styled/Theme.tsx
+++ src/components/styled/Theme.tsx
@@ -6,4 +6,11 @@ declare module 'styled-components' {
 
 export const Theme = {
    breakpoints: { mobile: '', tablet: '', desktop: '' },
+   colors: {
+      brightRed: '#FC4747',
+      deepBlue: '#10141E',
+      darkBlue: '#161D2F',
+      lightBlue: '#5A698F',
+      white: '#FFFFFF',
+   },
 }
