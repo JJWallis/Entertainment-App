5f5a1a841a786344a25fe2cdd6f400d96b0451f2
JJWallis <josh.jamesonwallis@gmail.com>
2022-08-31 20:25:25 +0100

    login screen ip

--- src/components/Login/LoginScreen.tsx
+++ src/components/Login/LoginScreen.tsx
@@ -1,7 +1,36 @@
+import Image from 'next/image'
 import React from 'react'
+import Logo from '../../assets/logo.svg'
+import { LoginButton } from '../styled/Button.styled'
+import LoginForm from '../styled/Form.styled'
+import { LoginContainer, LoginLogoContainer } from '../styled/Wrappers.styled'
 
 const LoginScreen: React.FC = () => {
-   return <></>
+   return (
+      <LoginContainer>
+         <LoginLogoContainer>
+            <Image src={Logo} alt="" width="35px" height="35px" />
+         </LoginLogoContainer>
+         <LoginForm>
+            <h1>Sign Up</h1>
+            <fieldset>
+               <div>
+                  <label>Email address</label>
+                  <input type="email" />
+               </div>
+               <div>
+                  <label>Password</label>
+                  <input type="password" />
+               </div>
+               <div>
+                  <label>Repeat password</label>
+                  <input type="password" />
+               </div>
+               <LoginButton>Create an account</LoginButton>
+            </fieldset>
+         </LoginForm>
+      </LoginContainer>
+   )
 }
 
 export default LoginScreen
