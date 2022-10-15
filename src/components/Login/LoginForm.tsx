366d3ef11779dfc51dcdb2505d27d15f34869ce6
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-15 17:38:20 +0100

    infinite loop fixed

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from 'react';
+import { LoginTitle } from '../styled/Title.styled';
+import LoginFormStyled from '../styled/Form.styled';
+import { LoginFieldSet } from '../styled/Fieldset.styled';
+import { LoginInputContainer } from '../styled/Wrappers.styled';
+import { LoginInput } from '../styled/Input.styled';
+import { LoginErrorMessage } from '../styled/ErrorMessage.styled';
+import { LoginButton } from '../styled/Button.styled';
+import { LoginSubTitle } from '../styled/SubTitle.styled';
+
+type FormStateValidation = Partial<InitialFormState>;
+
+interface InitialFormState {
+   email: string;
+   password: string;
+   confirmedPassword?: string; // optional required for delete operator
+}
+
+interface Props {
+   toggleFormType: () => void;
+   isSignUpForm: boolean;
+}
+
+const REGEXP_PASSWORD = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/;
+const REGEXP_EMAIL =
+   /^(?=(.{1,64}@.{1,255}))([!#$%&amp;'*+\-\/=?\^_`{|}~a-zA-Z0-9}]{1,64}(\.[!#$%&amp;'*+\-\/=?\^_`{|}~a-zA-Z0-9]{0,}){0,})@((\[(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\])|([a-zA-Z0-9-]{1,63}(\.[a-zA-Z0-9-]{2,63}){1,}))$/;
+
+const INITIAL_STATE = {
+   email: '',
+   password: '',
+};
+
+const initState = (isSignUpForm: boolean) => ({
+   ...INITIAL_STATE,
+   ...(isSignUpForm ? { confirmedPassword: '' } : {}),
+});
+
+const LoginForm = ({ toggleFormType, isSignUpForm }: Props) => {
+   const [userDetails, setUserDetails] = useState<InitialFormState>(
+      initState(isSignUpForm)
+   );
+   const [errorMessages, setErrorMessages] = useState<InitialFormState>(
+      initState(isSignUpForm)
+   );
+
+   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) =>
+      e.preventDefault();
+
+   useEffect(() => {
+      const validateCurrentField = (key: string, value: string) => {
+         switch (key) {
+            case 'email': {
+               if (value.length === 0) return '';
+               if (!REGEXP_EMAIL.test(value))
+                  return 'Please enter a valid email';
+               break;
+            }
+            case 'password': {
+               if (value.length === 0) return '';
+               if (!REGEXP_PASSWORD.test(value))
+                  return 'Please enter a valid password';
+               break;
+            }
+            case 'confirmedPassword': {
+               if (value.length === 0) return '';
+               if (value !== userDetails.password)
+                  return "Passwords don't match";
+               break;
+            }
+            default:
+               return '';
+         }
+         return '';
+      };
+
+      const userDetailsCopy = { ...userDetails };
+
+      const newErrorMessages: FormStateValidation = Object.entries(
+         userDetailsCopy
+      ).reduce((acc, [key, value]) => {
+         const currentErrorMessage = validateCurrentField(key, value);
+         return { ...acc, [key]: currentErrorMessage };
+      }, {});
+
+      const isErrorMessagesDifferent =
+         JSON.stringify(errorMessages) !== JSON.stringify(newErrorMessages);
+
+      console.log({ userDetails });
+
+      if (isErrorMessagesDifferent)
+         setErrorMessages(newErrorMessages as InitialFormState);
+   }, [userDetails, isSignUpForm, errorMessages]);
+
+   return (
+      <LoginFormStyled onSubmit={handleFormSubmit}>
+         <LoginTitle>{isSignUpForm ? 'Sign Up' : 'Login'}</LoginTitle>
+         <LoginFieldSet>
+            <LoginInputContainer>
+               <LoginInput
+                  data-testid="login-email-field"
+                  type="email"
+                  value={userDetails.email}
+                  onChange={(evt) =>
+                     setUserDetails({
+                        ...userDetails,
+                        ['email']: evt.target.value,
+                     })
+                  }
+                  placeholder="Email address"
+                  error={!!errorMessages.email}
+               />
+               {errorMessages.email && (
+                  <LoginErrorMessage data-testid="login-email-error">
+                     {errorMessages.email}
+                  </LoginErrorMessage>
+               )}
+            </LoginInputContainer>
+            <LoginInputContainer>
+               <LoginInput
+                  type="password"
+                  value={userDetails.password}
+                  onChange={(evt) =>
+                     setUserDetails({
+                        ...userDetails,
+                        ['password']: evt.target.value,
+                     })
+                  }
+                  placeholder="Password"
+                  error={!!errorMessages.password}
+               />
+               {errorMessages.password && (
+                  <LoginErrorMessage>
+                     {errorMessages.password}
+                  </LoginErrorMessage>
+               )}
+            </LoginInputContainer>
+            {isSignUpForm && (
+               <LoginInputContainer>
+                  <LoginInput
+                     type="password"
+                     value={userDetails.confirmedPassword}
+                     onChange={(evt) =>
+                        setUserDetails({
+                           ...userDetails,
+                           ['confirmedPassword']: evt.target.value,
+                        })
+                     }
+                     placeholder="Repeat password"
+                     error={!!errorMessages.confirmedPassword}
+                  />
+                  {errorMessages.confirmedPassword && (
+                     <LoginErrorMessage>
+                        {errorMessages.confirmedPassword}
+                     </LoginErrorMessage>
+                  )}
+               </LoginInputContainer>
+            )}
+         </LoginFieldSet>
+         <LoginButton>
+            {isSignUpForm ? 'Create an account' : 'Login to your account'}
+         </LoginButton>
+         <LoginSubTitle>
+            {isSignUpForm
+               ? 'Already have an account?'
+               : "Don't have an account?"}
+            <button type="button" onClick={toggleFormType}>
+               {isSignUpForm ? 'Login' : 'Sign Up'}
+            </button>
+         </LoginSubTitle>
+      </LoginFormStyled>
+   );
+};
+
+export default LoginForm;
--- src/components/Login/LoginScreen.tsx
+++ src/components/Login/LoginScreen.tsx
@@ -1,192 +1,37 @@
-import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
-import Logo from '../../assets/logo.svg'
-import { LoginButton } from '../styled/Button.styled'
-import { LoginErrorMessage } from '../styled/ErrorMessage.styled'
-import { LoginFieldSet } from '../styled/Fieldset.styled'
-import LoginForm from '../styled/Form.styled'
-import { LoginInput } from '../styled/Input.styled'
-import { LoginSubTitle } from '../styled/SubTitle.styled'
-import { LoginTitle } from '../styled/Title.styled'
-import {
-   LoginContainer,
-   LoginInputContainer,
-   LoginLogoContainer,
-} from '../styled/Wrappers.styled'
+import Image from 'next/image';
+import React, { useState } from 'react';
+import Logo from '../../assets/logo.svg';
+import LoginForm from './LoginForm';
+import { LoginContainer, LoginLogoContainer } from '../styled/Wrappers.styled';
 
-// TODO
-// https://github.com/reactjs/rfcs/blob/main/text/0213-suspense-in-react-18.md -> suspense for loading state
-
-type FormType = 'signUp' | 'login'
-type FormStateValidation = Partial<InitialFormState>
-
-interface InitialFormState {
-   email: string
-   password: string
-   confirmedPassword?: string // optional required for delete operator
-}
-
-const REGEXP_PASSWORD = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/
-const REGEXP_EMAIL =
-   /^(?=(.{1,64}@.{1,255}))([!#$%&amp;'*+\-\/=?\^_`{|}~a-zA-Z0-9}]{1,64}(\.[!#$%&amp;'*+\-\/=?\^_`{|}~a-zA-Z0-9]{0,}){0,})@((\[(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\])|([a-zA-Z0-9-]{1,63}(\.[a-zA-Z0-9-]{2,63}){1,}))$/
-
-const INITIAL_STATE = {
-   email: '',
-   password: '',
-   confirmedPassword: '',
-}
+type FormType = 'signUp' | 'login';
 
 const LoginScreen: React.FC = () => {
-   const [formType, setFormType] = useState<FormType>('signUp')
-   const [userDetails, setUserDetails] =
-      useState<InitialFormState>(INITIAL_STATE)
-   const [errorMessages, setErrorMessages] =
-      useState<InitialFormState>(INITIAL_STATE)
-   const isSignUpForm = formType === 'signUp'
+   const [formType, setFormType] = useState<FormType>('signUp');
+   const isSignUpForm = formType === 'signUp';
 
-   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) =>
-      e.preventDefault()
-
-   useEffect(() => {
-      const validateCurrentField = (key: string, value: string) => {
-         switch (key) {
-            case 'email': {
-               if (value.length === 0) return ''
-               if (!REGEXP_EMAIL.test(value))
-                  return 'Please enter a valid email'
-               break
-            }
-            case 'password': {
-               if (value.length === 0) return ''
-               if (!REGEXP_PASSWORD.test(value))
-                  return 'Please enter a valid password'
-               break
-            }
-            case 'confirmedPassword': {
-               if (value.length === 0) return ''
-               if (value !== userDetails.password)
-                  return "Passwords don't match"
-               break
-            }
-            default:
-               return ''
-         }
-         return ''
-      }
-
-      const userDetailsCopy = { ...userDetails }
-      if (!isSignUpForm) delete userDetailsCopy.confirmedPassword
-
-      const newErrorMessages: FormStateValidation = Object.entries(
-         userDetailsCopy
-      ).reduce((acc, [key, value]) => {
-         const currentErrorMessage = validateCurrentField(key, value)
-         return { ...acc, [key]: currentErrorMessage }
-      }, {})
-
-      const hasConfirmedPassword = 'confirmedPassword' in newErrorMessages
-
-      if (!hasConfirmedPassword) {
-         newErrorMessages.confirmedPassword = ''
-         setUserDetails((prevUserDetails) => ({
-            ...prevUserDetails,
-            ['confirmedPassword']: '',
-         }))
-      }
-
-      const isErrorMessagesDifferent =
-         JSON.stringify(errorMessages) !== JSON.stringify(newErrorMessages)
-
-      if (isErrorMessagesDifferent)
-         setErrorMessages(newErrorMessages as InitialFormState)
-   }, [userDetails, isSignUpForm, errorMessages])
+   const toggleFormType = () => setFormType(isSignUpForm ? 'login' : 'signUp');
 
    return (
       <LoginContainer>
          <LoginLogoContainer>
             <Image src={Logo} alt="" width="35px" height="35px" />
          </LoginLogoContainer>
-         <LoginForm onSubmit={handleFormSubmit}>
-            <LoginTitle>{isSignUpForm ? 'Sign Up' : 'Login'}</LoginTitle>
-            <LoginFieldSet>
-               <LoginInputContainer>
-                  <LoginInput
-                     data-testid="login-email-field"
-                     type="email"
-                     value={userDetails.email}
-                     onChange={(evt) =>
-                        setUserDetails({
-                           ...userDetails,
-                           ['email']: evt.target.value,
-                        })
-                     }
-                     placeholder="Email address"
-                     error={!!errorMessages.email}
-                  />
-                  {errorMessages.email && (
-                     <LoginErrorMessage data-testid="login-email-error">
-                        {errorMessages.email}
-                     </LoginErrorMessage>
-                  )}
-               </LoginInputContainer>
-               <LoginInputContainer>
-                  <LoginInput
-                     type="password"
-                     value={userDetails.password}
-                     onChange={(evt) =>
-                        setUserDetails({
-                           ...userDetails,
-                           ['password']: evt.target.value,
-                        })
-                     }
-                     placeholder="Password"
-                     error={!!errorMessages.password}
-                  />
-                  {errorMessages.password && (
-                     <LoginErrorMessage>
-                        {errorMessages.password}
-                     </LoginErrorMessage>
-                  )}
-               </LoginInputContainer>
-               {isSignUpForm && (
-                  <LoginInputContainer>
-                     <LoginInput
-                        type="password"
-                        value={userDetails.confirmedPassword}
-                        onChange={(evt) =>
-                           setUserDetails({
-                              ...userDetails,
-                              ['confirmedPassword']: evt.target.value,
-                           })
-                        }
-                        placeholder="Repeat password"
-                        error={!!errorMessages.confirmedPassword}
-                     />
-                     {errorMessages.confirmedPassword && (
-                        <LoginErrorMessage>
-                           {errorMessages.confirmedPassword}
-                        </LoginErrorMessage>
-                     )}
-                  </LoginInputContainer>
-               )}
-            </LoginFieldSet>
-            <LoginButton>
-               {isSignUpForm ? 'Create an account' : 'Login to your account'}
-            </LoginButton>
-            <LoginSubTitle>
-               {isSignUpForm
-                  ? 'Already have an account?'
-                  : "Don't have an account?"}
-               <button
-                  type="button"
-                  onClick={() => setFormType(isSignUpForm ? 'login' : 'signUp')}
-               >
-                  {isSignUpForm ? 'Login' : 'Sign Up'}
-               </button>
-            </LoginSubTitle>
-         </LoginForm>
+         {isSignUpForm ? (
+            <LoginForm
+               key="signUpForm"
+               toggleFormType={toggleFormType}
+               isSignUpForm={isSignUpForm}
+            />
+         ) : (
+            <LoginForm
+               key="loginForm"
+               toggleFormType={toggleFormType}
+               isSignUpForm={isSignUpForm}
+            />
+         )}
       </LoginContainer>
-   )
-}
+   );
+};
 
-export default LoginScreen
+export default LoginScreen;
