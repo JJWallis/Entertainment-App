6c2b9316a26c3d2af8a15e83f512effdeda440b7
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-21 21:18:56 +0100

    useDebounce added

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,10 +1,12 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
-export const useDebounce = (fn: () => void) => {
-   const timeoutRef = useRef<NodeJS.Timeout>(null);
+export const useDebounce = <T,>(fn: () => void, deps: T[]) => {
+   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
    const debouncedFunction = () => {
-      clearTimeout(timeoutRef.current);
+      clearTimeout(timeoutRef.current || undefined);
       timeoutRef.current = setTimeout(fn, 500);
    };
+
+   useEffect(debouncedFunction, deps);
 };
