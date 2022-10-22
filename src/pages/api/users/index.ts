1869294e70bcfd2ea1dc3948c5dfa69f5956115b
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-22 09:27:51 +0100

    user api setup - return all users

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { User } from '../../../types/User.interface';
+import users from '../../../users';
+
+export default function handler(
+   _req: NextApiRequest,
+   res: NextApiResponse<User[]>
+) {
+   return res.status(200).json(users);
+}
---
+++ src/types/User.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+   id: string;
+   firstName: string;
+   lastName: string;
+   password: string;
+   profileImage: string;
+   email: string;
+}
