7bb834339fcc465242d8e78a39fcc01a1cbcf876
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-23 08:58:03 +0100

    user json added

--- src/pages/api/users/sign-up.ts
+++ src/pages/api/users/sign-up.ts
@@ -1,15 +1,25 @@
+import fs from 'fs';
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import { v4 as uuid } from 'uuid';
 
 import { User, UserWithoutId } from '../../../types/User.interface';
-import users from '../../../users';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-   if (req.method === 'POST') {
-      const id = uuid();
-      const newUser: User = { id, ...(req.body as UserWithoutId) };
+export default async function handler(
+   req: NextApiRequest,
+   res: NextApiResponse
+) {
+   try {
+      if (req.method === 'POST') {
+         const id = uuid();
+         const newUser: User = { id, ...(req.body as UserWithoutId) };
 
-      users.push(newUser);
-      res.status(200).json({ message: 'User created successfully' });
+         users.push(newUser);
+         res.status(200).json({ message: 'User created successfully' });
+      }
+   } catch (error: unknown) {
+      console.error(error);
+      if (error instanceof Error)
+         res.status(400).json({ message: error.message });
    }
 }
